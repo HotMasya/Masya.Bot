09580dafe3f4a1fffe6af7f43528ad22c5dc17b6
Masya <i.am.masya1@gmail.com>
2022-06-11 14:40:56 +0300

    updated Program.cs

--- Masya.Bot.ConsoleUI/Program.cs
+++ Masya.Bot.ConsoleUI/Program.cs
@@ -5,38 +5,6 @@ string token = Environment.GetEnvironmentVariable("BOT_TOKEN");
 
 using var client = new RestApiClient(token);
 
-await client.GetUserAsync(236426208315834369);
-await client.GetUserAsync(236426208315834369);
-await client.GetUserAsync(236426208315834369);
-await client.GetUserAsync(236426208315834369);
-await client.GetUserAsync(236426208315834369);
-await client.GetUserAsync(236426208315834369);
-await client.GetUserAsync(236426208315834369);
-await client.GetUserAsync(236426208315834369);
-await client.GetUserAsync(236426208315834369);
-await client.GetUserAsync(236426208315834369);
-await client.GetUserAsync(236426208315834369);
-await client.GetUserAsync(236426208315834369);
-await client.GetUserAsync(236426208315834369);
-await client.GetUserAsync(236426208315834369);
-await client.GetUserAsync(236426208315834369);
-await client.GetUserAsync(236426208315834369);
-await client.GetUserAsync(236426208315834369);
-await client.GetUserAsync(236426208315834369);
-await client.GetUserAsync(236426208315834369);
-await client.GetUserAsync(236426208315834369);
-await client.GetUserAsync(236426208315834369);
-await client.GetUserAsync(236426208315834369);
-await client.GetUserAsync(236426208315834369);
-await client.GetUserAsync(236426208315834369);
-await client.GetUserAsync(236426208315834369);
-await client.GetUserAsync(236426208315834369);
-await client.GetUserAsync(236426208315834369);
-await client.GetUserAsync(236426208315834369);
-await client.GetUserAsync(236426208315834369);
-await client.GetUserAsync(236426208315834369);
-await client.GetUserAsync(236426208315834369);
+var user = await client.GetUserAsync(236426208315834369);
 
-var user2 = await client.GetUserAsync(324416295120404480);
-
-Console.WriteLine(user2?.Username);
+Console.WriteLine(user?.Username);
